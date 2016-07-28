8373c27e9fe74877a633ee7a7840e4383f6c26d5
Xiaoyang Kao <contact.xykao@gmail.com>
2016-07-29 04:18:06 +0800

    More shit

---
+++ php-i18n/langcache/php_i18n_6b988d08aa48d369fb87a1773a26f363_L_en.cache.php
@@ -0,0 +1,11 @@
+<?php class L {
+const greeting = 'Hello World!';
+const category_somethingother = 'Something other...';
+public static function __callStatic($string, $args) {
+    return vsprintf(constant("self::" . $string), $args);
+}
+}
+function L($string, $args=NULL) {
+    $return = constant("L::".$string);
+    return $args ? vsprintf($return,$args) : $return;
+}
