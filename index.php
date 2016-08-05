79473c06c7a63fac3b73d803c6fe7e62a8395824
Xiaoyang Kao <contact.xykao@gmail.com>
2016-08-05 16:03:00 +0800

    Compression, compiled CSS, shifted JS load order

--- index.php
+++ index.php
@@ -12,13 +12,9 @@
 		<title><?php echo L::title; ?></title>	
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">			
-		<link rel="stylesheet/less" type="text/css" href="/style.less"/>
+		<link rel="stylesheet" type="text/css" href="/css/style.css"/>
 		<link id="favicon" rel="shortcut icon" type="image/x-icon" href="/img/favicon.png" />
 
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js"></script>
-		<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
-		<script src="js/jquery.countdown.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 		<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
 
@@ -33,135 +29,6 @@
 		  }}();
 		</script>
 
-		<script>
-		/**
-		 * @author       Rob W <gwnRob@gmail.com>
-		 * @website      http://stackoverflow.com/a/7513356/938089
-		 * @version      20131010
-		 * @description  Executes function on a framed YouTube video (see website link)
-		 *               For a full list of possible functions, see:
-		 *               https://developers.google.com/youtube/js_api_reference
-		 * @param String frame_id The id of (the div containing) the frame
-		 * @param String func     Desired function to call, eg. "playVideo"
-		 *        (Function)      Function to call when the player is ready.
-		 * @param Array  args     (optional) List of arguments to pass to function func*/
-		function callPlayer(frame_id, func, args) {
-		    if (window.jQuery && frame_id instanceof jQuery) frame_id = frame_id.get(0).id;
-		    var iframe = document.getElementById(frame_id);
-		    if (iframe && iframe.tagName.toUpperCase() != 'IFRAME') {
-		        iframe = iframe.getElementsByTagName('iframe')[0];
-		    }
-
-		    // When the player is not ready yet, add the event to a queue
-		    // Each frame_id is associated with an own queue.
-		    // Each queue has three possible states:
-		    //  undefined = uninitialised / array = queue / 0 = ready
-		    if (!callPlayer.queue) callPlayer.queue = {};
-		    var queue = callPlayer.queue[frame_id],
-		        domReady = document.readyState == 'complete';
-
-		    if (domReady && !iframe) {
-		        // DOM is ready and iframe does not exist. Log a message
-		        window.console && console.log('callPlayer: Frame not found; id=' + frame_id);
-		        if (queue) clearInterval(queue.poller);
-		    } else if (func === 'listening') {
-		        // Sending the "listener" message to the frame, to request status updates
-		        if (iframe && iframe.contentWindow) {
-		            func = '{"event":"listening","id":' + JSON.stringify(''+frame_id) + '}';
-		            iframe.contentWindow.postMessage(func, '*');
-		        }
-		    } else if (!domReady ||
-		               iframe && (!iframe.contentWindow || queue && !queue.ready) ||
-		               (!queue || !queue.ready) && typeof func === 'function') {
-		        if (!queue) queue = callPlayer.queue[frame_id] = [];
-		        queue.push([func, args]);
-		        if (!('poller' in queue)) {
-		            // keep polling until the document and frame is ready
-		            queue.poller = setInterval(function() {
-		                callPlayer(frame_id, 'listening');
-		            }, 250);
-		            // Add a global "message" event listener, to catch status updates:
-		            messageEvent(1, function runOnceReady(e) {
-		                if (!iframe) {
-		                    iframe = document.getElementById(frame_id);
-		                    if (!iframe) return;
-		                    if (iframe.tagName.toUpperCase() != 'IFRAME') {
-		                        iframe = iframe.getElementsByTagName('iframe')[0];
-		                        if (!iframe) return;
-		                    }
-		                }
-		                if (e.source === iframe.contentWindow) {
-		                    // Assume that the player is ready if we receive a
-		                    // message from the iframe
-		                    clearInterval(queue.poller);
-		                    queue.ready = true;
-		                    messageEvent(0, runOnceReady);
-		                    // .. and release the queue:
-		                    while (tmp = queue.shift()) {
-		                        callPlayer(frame_id, tmp[0], tmp[1]);
-		                    }
-		                }
-		            }, false);
-		        }
-		    } else if (iframe && iframe.contentWindow) {
-		        // When a function is supplied, just call it (like "onYouTubePlayerReady")
-		        if (func.call) return func();
-		        // Frame exists, send message
-		        iframe.contentWindow.postMessage(JSON.stringify({
-		            "event": "command",
-		            "func": func,
-		            "args": args || [],
-		            "id": frame_id
-		        }), "*");
-		    }
-		    /* IE8 does not support addEventListener... */
-		    function messageEvent(add, listener) {
-		        var w3 = add ? window.addEventListener : window.removeEventListener;
-		        w3 ?
-		            w3('message', listener, !1)
-		        :
-		            (add ? window.attachEvent : window.detachEvent)('onmessage', listener);
-		    }
-		}
-
-			$(document).ready(function() {
-				$('.internalNav a').click(function(){
-					$('html, body').animate({
-				        scrollTop: $( $(this).attr('href') ).offset().top
-				    }, 300, 'swing');
-				    return false;
-				});
-
-				$('.primary-nav a').click(function(){
-				    $('html, body').animate({
-				        scrollTop: $( $(this).attr('href') ).offset().top
-				    }, 300, 'swing');
-				    return false;
-				});
-
-				$(".nav-button").click(function () {
-					$(".nav-button,.primary-nav").toggleClass("open");
-				});
-				$("#days").countdown("2016/09/13", function(event) {
-				  $(this).text(event.strftime('%D'));
-				});
-				$("#hours").countdown("2016/09/13", function(event) {
-				  $(this).text(event.strftime('%H'));
-				});
-				$("#minutes").countdown("2016/09/13", function(event) {
-				  $(this).text(event.strftime('%M'));
-				});
-				$("#seconds").countdown("2016/09/13", function(event) {
-				  $(this).text(event.strftime('%S'));
-				});
-
-				$('.stream_modal').on('hidden.bs.modal', function () {
-				    $(".stream_modal iframe").attr("src", $(".stream_modal iframe").attr("src"));
-				});
-
-			});		
-		
-		</script>
 	</head>
 
 	<body>
@@ -499,6 +366,138 @@
 
 
 
+		<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+		<script src="js/jquery.countdown.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+		<script>
+		/**
+		 * @author       Rob W <gwnRob@gmail.com>
+		 * @website      http://stackoverflow.com/a/7513356/938089
+		 * @version      20131010
+		 * @description  Executes function on a framed YouTube video (see website link)
+		 *               For a full list of possible functions, see:
+		 *               https://developers.google.com/youtube/js_api_reference
+		 * @param String frame_id The id of (the div containing) the frame
+		 * @param String func     Desired function to call, eg. "playVideo"
+		 *        (Function)      Function to call when the player is ready.
+		 * @param Array  args     (optional) List of arguments to pass to function func*/
+		function callPlayer(frame_id, func, args) {
+		    if (window.jQuery && frame_id instanceof jQuery) frame_id = frame_id.get(0).id;
+		    var iframe = document.getElementById(frame_id);
+		    if (iframe && iframe.tagName.toUpperCase() != 'IFRAME') {
+		        iframe = iframe.getElementsByTagName('iframe')[0];
+		    }
+
+		    // When the player is not ready yet, add the event to a queue
+		    // Each frame_id is associated with an own queue.
+		    // Each queue has three possible states:
+		    //  undefined = uninitialised / array = queue / 0 = ready
+		    if (!callPlayer.queue) callPlayer.queue = {};
+		    var queue = callPlayer.queue[frame_id],
+		        domReady = document.readyState == 'complete';
+
+		    if (domReady && !iframe) {
+		        // DOM is ready and iframe does not exist. Log a message
+		        window.console && console.log('callPlayer: Frame not found; id=' + frame_id);
+		        if (queue) clearInterval(queue.poller);
+		    } else if (func === 'listening') {
+		        // Sending the "listener" message to the frame, to request status updates
+		        if (iframe && iframe.contentWindow) {
+		            func = '{"event":"listening","id":' + JSON.stringify(''+frame_id) + '}';
+		            iframe.contentWindow.postMessage(func, '*');
+		        }
+		    } else if (!domReady ||
+		               iframe && (!iframe.contentWindow || queue && !queue.ready) ||
+		               (!queue || !queue.ready) && typeof func === 'function') {
+		        if (!queue) queue = callPlayer.queue[frame_id] = [];
+		        queue.push([func, args]);
+		        if (!('poller' in queue)) {
+		            // keep polling until the document and frame is ready
+		            queue.poller = setInterval(function() {
+		                callPlayer(frame_id, 'listening');
+		            }, 250);
+		            // Add a global "message" event listener, to catch status updates:
+		            messageEvent(1, function runOnceReady(e) {
+		                if (!iframe) {
+		                    iframe = document.getElementById(frame_id);
+		                    if (!iframe) return;
+		                    if (iframe.tagName.toUpperCase() != 'IFRAME') {
+		                        iframe = iframe.getElementsByTagName('iframe')[0];
+		                        if (!iframe) return;
+		                    }
+		                }
+		                if (e.source === iframe.contentWindow) {
+		                    // Assume that the player is ready if we receive a
+		                    // message from the iframe
+		                    clearInterval(queue.poller);
+		                    queue.ready = true;
+		                    messageEvent(0, runOnceReady);
+		                    // .. and release the queue:
+		                    while (tmp = queue.shift()) {
+		                        callPlayer(frame_id, tmp[0], tmp[1]);
+		                    }
+		                }
+		            }, false);
+		        }
+		    } else if (iframe && iframe.contentWindow) {
+		        // When a function is supplied, just call it (like "onYouTubePlayerReady")
+		        if (func.call) return func();
+		        // Frame exists, send message
+		        iframe.contentWindow.postMessage(JSON.stringify({
+		            "event": "command",
+		            "func": func,
+		            "args": args || [],
+		            "id": frame_id
+		        }), "*");
+		    }
+		    /* IE8 does not support addEventListener... */
+		    function messageEvent(add, listener) {
+		        var w3 = add ? window.addEventListener : window.removeEventListener;
+		        w3 ?
+		            w3('message', listener, !1)
+		        :
+		            (add ? window.attachEvent : window.detachEvent)('onmessage', listener);
+		    }
+		}
+
+			$(document).ready(function() {
+				$('.internalNav a').click(function(){
+					$('html, body').animate({
+				        scrollTop: $( $(this).attr('href') ).offset().top
+				    }, 300, 'swing');
+				    return false;
+				});
+
+				$('.primary-nav a').click(function(){
+				    $('html, body').animate({
+				        scrollTop: $( $(this).attr('href') ).offset().top
+				    }, 300, 'swing');
+				    return false;
+				});
+
+				$(".nav-button").click(function () {
+					$(".nav-button,.primary-nav").toggleClass("open");
+				});
+				$("#days").countdown("2016/09/13", function(event) {
+				  $(this).text(event.strftime('%D'));
+				});
+				$("#hours").countdown("2016/09/13", function(event) {
+				  $(this).text(event.strftime('%H'));
+				});
+				$("#minutes").countdown("2016/09/13", function(event) {
+				  $(this).text(event.strftime('%M'));
+				});
+				$("#seconds").countdown("2016/09/13", function(event) {
+				  $(this).text(event.strftime('%S'));
+				});
+
+				$('.stream_modal').on('hidden.bs.modal', function () {
+				    $(".stream_modal iframe").attr("src", $(".stream_modal iframe").attr("src"));
+				});
+
+			});		
+		
+		</script>
 	</body>
 </html>
