b2fc3f4febbad05903e01adbef408e06a5328739
Xiaoyang Kao <contact.xykao@gmail.com>
2016-08-12 18:40:43 +0800

    Git deploy

--- git.php
+++ git.php
@@ -1,13 +1,6 @@
 <?php
 
-define('PRIVATE_KEY', 'XXXXXXXXXXXXXXXXxxx');
-
 echo shell_exec("git pull");
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST'
-        && $_REQUEST['thing'] === PRIVATE_KEY)
-{
-    echo shell_exec("git pull");
-}
+echo shell_exec("rm -r ./langcache/*");
 
 ?>
